0c3e0f9962b4cb1d1d329b9e1bd43b6dd5bb79f5
Brett Sumser <bsumser@gmail.com>
2022-08-25 13:58:26 -0700

    added local host server for testing

---
+++ local.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var mysql = require('mysql');
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+//// Create a connection to the database
+//const connection = mysql.createConnection({
+//  host: 'localhost',
+//  user: 'keeper',
+//  password: 'Chronicler_666',
+//  database: "web"       
+//});
+
+// open the MySQL connection
+//connection.connect(error => {
+//    if (error){
+//        console.log("A error has been occurred "
+//            + "while connecting to database.");       
+//        throw error;
+//    }
+//     
+//    //If Everything goes correct, Then start Express Server
+//    app.listen(PORT, ()=>{
+//        console.log("Database connection is Ready and "
+//             + "Server is Listening on Port ", PORT);
+//    })
+//});
+//
+//module.exports = connection; //connection for use in other scripts
+
+var server = app.listen(8000, function(error){
+    var port = server.address().port;
+
+    if (!error)
+        console.log("Server started at http://localhost:%s", port);
+    else
+        console.log("Error occurred, server cant start", error)
+});
+
