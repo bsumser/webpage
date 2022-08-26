901809ad3adc4ba70481d53a20d36f060e9dc1be
Brett Sumser <bsumser@gmail.com>
2022-08-25 22:02:18 -0700

    working example via the link below
    "https://blog.logrocket.com/data-visualization-d3-js-node-js/"

---
+++ public/pages/graphTest.js
@@ -0,0 +1,9 @@
+//import * as data from './graph.json';
+//
+//const {name} = data;
+
+function loadGraph(){
+    //var graphData = loadJSON();
+    console.log("loadGraph called");
+    //ForceGraph(data);
+}
---
+++ public/test.js
@@ -0,0 +1,7 @@
+console.log('It works!');
+
+window.onload=function(){
+    document.getElementById("BT1").addEventListener("click", function(){
+        alert("oh snap, i was clicked...");
+    });
+}
