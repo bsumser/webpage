4d2884244462f1e26a781218dfb8345e962f3011
Brett Sumser <bsumser@gmail.com>
2023-06-24 23:51:27 -0700

    query to database change

--- public/pages/test.js
+++ public/pages/test.js
@@ -1,7 +1,17 @@
-console.log('It works!');
+const connection = require('../../myapp.js');
 
-window.onload=function(){
-    document.getElementById("BT1").addEventListener("click", function(){
-        alert("oh snap, i was clicked...");
+function performQuery() {
+    var query = document.getElementById("name_entry").value;    //get text box 
+    document.getElementById("demo").innerHTML = query;
+
+    console.log(query);     //log query for debug
+
+    query = "SELECT * FROM hi_score WHERE SCORE=" + query   //concat query with score
+    connection.query(query, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
     });
 }
+
+
+connection.end();
