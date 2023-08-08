dfc6da60cf5ca93bb9941edbaaa536d9038bc53d
Brett Sumser <bsumser@gmail.com>
2023-08-08 16:26:04 -0700

    logging query

--- public/pages/test.js
+++ public/pages/test.js
@@ -1,12 +1,14 @@
 const connection = require('../../myapp.js');
 
-function performQuery() {
-    var query = document.getElementById("name_entry").value;    //get text box 
+function performQuery(value) {
+    var query = document.getElementById("score_entry").value;    //get text box 
     document.getElementById("demo").innerHTML = query;
 
     console.log(query);     //log query for debug
 
     query = "SELECT * FROM hi_score WHERE SCORE=" + query   //concat query with score
+    console.log(query);     //log query for debug
+    
     connection.query(query, function (err, result, fields) {
         if (err) throw err;
         console.log(result);
