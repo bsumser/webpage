8bd0cf38cb489406564239e89c26ee5e0b714ff2
Brett Sumser <bsumser@gmail.com>
2023-08-09 17:08:12 -0700

    cleaned up unused code

--- public/pages/test.js
+++ public/pages/test.js
@@ -1,12 +1,9 @@
 //const connection = require('../../myapp.js');
 
 function performQuery(value) {
-    var query = document.getElementById("score_entry").value;    //get text box 
-    document.getElementById("demo").innerHTML = query;
+    let score = document.getElementById("score_entry").value;    //get text box 
 
-    console.log(query);     //log query for debug
-
-    query = "INSERT INTO hi_score" + value + "hi_score;"  //concat query with score
+    let query = "INSERT INTO hi_score(" + score + ");"  //concat query with score
     console.log(query);     //log query for debug
     
     connection.query(query, function (err, result, fields) {
