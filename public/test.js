c333ac284afaf1a513b24f3feac47125f2acbafa
Brett Sumser <bsumser@gmail.com>
2023-08-09 17:09:10 -0700

    remove unused files

--- public/test.js
+++
@@ -1,7 +0,0 @@
-console.log('It works!');
-
-window.onload=function(){
-    document.getElementById("BT1").addEventListener("click", function(){
-        alert("oh snap, i was clicked...");
-    });
-}
