f4fcb425299545662eae3c684d872c2655fadcc3
Brett Sumser <bsumser@gmail.com>
2023-09-26 13:48:08 -0700

    date loop working

--- scripts/fisher.py
+++ scripts/fisher.py
@@ -1,17 +1,31 @@
 import requests
+import pandas as pd
+
 
 def main():
+    url = "https://www.sandiegofishreports.com/dock_totals/boats.php?date="
+    
     # date format is:
     #2023-09-18
+    start_date = "2023-1-1"
+    end_date = "2023-6-25"
+    daterange = pd.date_range(start_date, end_date)
 
-    url = "https://www.sandiegofishreports.com/dock_totals/boats.php?date="
+    for single_date in daterange:
+        str_date = single_date.strftime("%Y-%m-%d")
+        url_concat = url + str_date
+        fish_call(url_concat, str_date)
 
-def fish_call(url):
+
+
+def fish_call(url, date):
     # scrape url data from san diego fish reports
 
-    page = requests.get(url)
+    print("fishing data from url - %s\n------day:%s\n" % (url, date))
 
-    print(page.text)
+    #page = requests.get(url)
+
+    #print(page.text)
 
 if __name__ == "__main__":
     main()
