4ebd6d4434b095375bc347e6965089adf61e1566
Brett Sumser <bsumser@gmail.com>
2023-11-28 17:06:55 -0800

    init server, mysql

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+const PORT = 3001;
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
