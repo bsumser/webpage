849bc55d78214ccb0f7f6f959b9c29a19c5a88c7
Brett Sumser <bsumser@gmail.com>
2024-05-04 14:01:04 -0700

    re-added project descriptions

--- src/components/home.js
+++ src/components/home.js
@@ -5,9 +5,24 @@ const Home = () => {
     return (
         <>
           <Project 
-          title="Neural Network"
-          summary="Project summary goes here"
-          image="./img/image.png"
+          title="Machine Learning Neural Network"
+
+          summary="The inspiration for this project was my experience working with wine in
+	    		a restaurant. I thought that making a neural network that was able to differentiate
+	    		between different varietals of wine would be educational for my future projects."
+
+          //image="./img/image.png"
+          />
+          
+          <Project 
+          title="C++ implementation of the Fourier Transform"
+
+          summary="This was a 2 person group project for a
+	    		course focused on high performance parallel computing. Different versions of fast fourier transform algorithms
+	    		were studied, and we attempted to implement various parallel computing technology such as
+	    		MPI, or OpenMP."
+
+          //image="./img/image.png"
           />
         </>
     )
