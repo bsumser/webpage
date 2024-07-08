14e4aca2dda443ce8265fc18f3af0a605796f035
Brett Sumser <bsumser@gmail.com>
2024-07-08 16:05:38 -0700

    new test component

---
+++ src/components/mtg.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+
+const MTG = () => {
+    //const [posts, setPosts] = useState([]);
+    useEffect(() => {
+       fetch('http://localhost:8080/', {method: ("GET")})
+          .then((response) => response.json())
+          .then((data) => {
+             console.log(data);
+          })
+          .catch((err) => {
+             console.log(err.message);
+          });
+    }, []);
+     return (
+         <h1>MTG TEST SITE</h1>
+    )
+}
+
+export default MTG
