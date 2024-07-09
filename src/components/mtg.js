662613089333c1e4342a30b712f8c0f45a5557c7
Brett Sumser <bsumser@gmail.com>
2024-07-09 16:10:28 -0700

    testing react text input

--- src/components/mtg.js
+++ src/components/mtg.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
-
-const MTG = () => {
+const MTG = (props: Props) => {
     //const [posts, setPosts] = useState([]);
-    useEffect(() => {
-       fetch('http://localhost:8080/deck?deck=Forest')
-          .then((response) => response.json())
-          .then((data) => {
-             console.log(data);
-          })
-          .catch((err) => {
-             console.log(err.message);
-          });
-    }, []);
+    const [deckText, setDeckText] = useState('');
+    //useEffect(() => {
+    //   fetch('http://localhost:8080/deck?deck=4%20Forest')
+    //      .then((response) => response.json())
+    //      .then((data) => {
+    //         console.log(data);
+    //      })
+    //      .catch((err) => {
+    //         console.log(err.message);
+    //      });
+    //}, []);
+  function onDeckTextChange(e){
+    setDeckText(e.target.value);
+  }
      return (
+    <div>
          <h1>MTG TEST SITE</h1>
+          <input type="text" value={deckText} onChange={onDeckTextChange}/>
+    </div>
     )
 }
 
