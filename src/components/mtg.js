df6194d7d2f6d42d2b4c921935325318cf57b226
Brett Sumser <bsumser@gmail.com>
2024-07-24 20:54:42 -0700

    changing form input method

--- src/components/mtg.js
+++ src/components/mtg.js
@@ -13,13 +13,44 @@ const MTG = (props: Props) => {
     //         console.log(err.message);
     //      });
     //}, []);
-  function onDeckTextChange(e){
-    setDeckText(e.target.value);
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    fetch('/some-api', { method: form.method, body: formData });
+    fetch('http://localhost:8080/', { method: form.method, body: formData })
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
   }
      return (
     <div>
          <h1>MTG TEST SITE</h1>
-          <input type="text" value={deckText} onChange={onDeckTextChange}/>
+         <form method="post" onSubmit={handleSubmit}>
+      <label>
+        <textarea
+          name="postContent"
+          defaultValue="Enter deck here"
+          rows={4}
+          cols={40}
+        />
+      </label>
+      <hr />
+      <button type="submit">Enter Deck</button>
+    </form>
     </div>
     )
 }
