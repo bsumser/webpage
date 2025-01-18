4b10c9b60f084d10452bc11e37fbdc93b554252e
Brett Sumser <bsumser@gmail.com>
2025-01-17 23:59:57 -0800

    work section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,9 +8,9 @@ const Main = () => {
         <div id='main'>
             <img className='w-full h-screen object-cover object-left' src={bg} alt="leaves" />
             <div className="w-full h-screen absolute top-0 left-0 bg-white/25">
-            <div>
-                <h1>I'm Brett Sumser</h1>
-                <h2>
+            <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+                <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Brett Sumser</h1>
+                <h2 className="flex sm:3xl text-2xl pt-4 text-gray-800">
                     I'm a 
                     <TypeAnimation
                       sequence={[
@@ -28,7 +28,7 @@ const Main = () => {
                       repeat={Infinity}
                     />
                 </h2>
-                <div>
+                <div className="flex justify-between pt-6 max-w-[200px] w-full">
                     <FaGithub className='cursor-pointer' size={20}/>
                     <FaInstagram className="cursor-pointer" size={20}/>
                 </div>
