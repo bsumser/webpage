40fefbb46ec71e82dd3c3cc7c5c65040a84dadf0
Brett Sumser <bsumser@gmail.com>
2025-01-20 21:33:36 -0800

    added component

---
+++ src/components/Photo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
+
+const Photo = () => {
+  return (
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Photo</h1>
+    </div>
+  )
+}
+
+export default Photo
