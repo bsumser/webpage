71a2e6194795796092f7bb0e764759dec9e56a67
Brett Sumser <bsumser@gmail.com>
2025-01-25 12:21:46 -0800

    rework app with router, mtg into own page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import { useState } from 'react';
-import Sidenav from './components/Sidenav';
-import Main from './components/Main';
-import Work from './components/Work';
-import Projects from './components/Projects';
-import Photo from './components/Photo';
+import Landing from './components/Landing';
 import MTG from './components/MTG';
-import DeckComponent from './components/DeckComponent'; // Import DeckComponent
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [deck, setDeck] = useState(null);
-
-  // Function to handle deck data from MTG component
-  const handleDeckData = (data) => {
-    setDeck(data); // Update the deck state
-  };
-
   return (
-    <div>
-      <Sidenav />
-      <Main />
-      <Work />
-      <Projects />
-      <MTG onDeckData={handleDeckData} /> {/* Pass handleDeckData as a prop to MTG */}
-      
-      {/* Render DeckComponent if deck data is available */}
-      {deck && <DeckComponent deck={deck} />}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/mtg" element={<MTG />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
