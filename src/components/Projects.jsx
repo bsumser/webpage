8869dda9db850c4ce31cb10dbe2c73d86b9500d6
Brett Sumser <bsumser@gmail.com>
2025-02-08 10:19:28 -0800

    changed image, and project text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-import propertyImage from '../assets/property.png'
+import quackImage from '../assets/duck.png'
 import aocImage from '../assets/aoc.png'
 import proj3img from '../assets/property.png'
 import proj4img from '../assets/property.png'
@@ -13,8 +13,8 @@ const Projects = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac posuere quam, quis aliquam tortor.
         </p>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={propertyImage} title='Quack' desc='' link='https://github.com/bsumser/tiny_vm'/>
-            <ProjectItem img={aocImage} title='Advent of Code' desc='' link='https://github.com/bsumser/AOC'/>
+            <ProjectItem img={quackImage} title='Quack' desc='Objected Oriented Programming Langauge' link='https://github.com/bsumser/tiny_vm'/>
+            <ProjectItem img={aocImage} title='Advent of Code' desc='Christmas themed programming competition!' link='https://github.com/bsumser/AOC'/>
             <ProjectItem img={proj3img} title='Neural Network' desc='' link='https://github.com/bsumser/neuralNetwork'/>
             <ProjectItem img={proj4img} title='GitHub' desc='' link='https://github.com/bsumser/tiny_vm'/>
         </div>
