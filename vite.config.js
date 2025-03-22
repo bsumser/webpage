98d9c9d9b28447a494cab22f40bb17c0f73ca08e
Brett Sumser <bsumser@gmail.com>
2025-03-22 09:15:47 -0700

    goatcounter

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-goatcounter',
+      transformIndexHtml(html) {
+        // Add the GoatCounter script tag before the closing </body> tag
+        return html.replace(
+          '</body>',
+          `
+            <script data-goatcounter="https://bsumser.goatcounter.com/count"
+              async src="//gc.zgo.at/count.js"></script>
+            </script>
+          </body>`
+        );
+      }
+    }
+  ],
 })
