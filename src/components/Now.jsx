4b06a008115b0f01621ebbe3f64fef01b8cf1b7c
Brett Sumser <bsumser@gmail.com>
2025-06-09 17:48:06 -0700

    now page
    added template now page

---
+++ src/components/Now.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+  
+const Now = () => {
+  return (
+    <div id='now' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+      <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Now</h1>
+        <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
+        </p>
+    </div>
+  );
+}
+
+export default Now;
