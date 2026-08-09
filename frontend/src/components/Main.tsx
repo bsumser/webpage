42f0424f99838a3c1ef004210215f244a1a75301
Brett Sumser <bsumser@gmail.com>
2026-08-09 12:23:05 +0900

    update Main and Work components: correct name and add WorkItem details

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -8,7 +8,7 @@ export default function Main() {
             <img className='w-full h-screen object-cover object-left' src={bg} alt="leaves" />
             <div className="w-full h-screen absolute top-0 left-0 bg-white/25">
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-                <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Brett Sumser</h1>
+                <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Thomas Sumser</h1>
                 <h2 className="flex sm:3xl text-2xl pt-4 text-gray-800">
                     I'm a 
                     <TypeAnimation
@@ -16,7 +16,7 @@ export default function Main() {
                         // Same substring at the start will only be typed out once, initially
                         'Developer',
                         2000, // wait 1s before replacing "Mice" with "Hamsters"
-                        'Coder',
+                        'Vim Enthusiast',
                         2000,
                         'Photographer',
                         2000,
